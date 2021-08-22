13034039c33a5edd743291c02a0ac114654f4b27
Sandesh Soni <sandeshsoni1@gmail.com>
2021-08-22 20:26:45 +0530

    INIT authentication app

---
+++ apps/authentication/lib/authentication.ex
@@ -0,0 +1,18 @@
+defmodule Authentication do
+  @moduledoc """
+  Documentation for `Authentication`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Authentication.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
